9d44e99b9beb2622f92f8fbb7c82c5badbfd1ecf
Faraz Khan <farazkhan138@gmail.com>
2020-05-03 23:54:06 +0530

    Create middleware.py

---
+++ learn/middleware.py
@@ -0,0 +1,32 @@
+def simple_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    return middleware
+    
+   #Class method middleware
+  class AnotherSimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
